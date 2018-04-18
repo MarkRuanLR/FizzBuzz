aaf813c17ff7a6ed7890bbf7ca7ec6e4c296fab8
strawls <mark_ruan@icloud.com>
2018-04-18 22:18:48 +0800

    finish

--- src/main/java/com/thoughtworks/FizzBuzzGame.java
+++ src/main/java/com/thoughtworks/FizzBuzzGame.java
@@ -8,9 +8,35 @@ public class FizzBuzzGame {
     private List<String> results = new ArrayList<>();
 
     public void start(int count) {
+        for (int i = 1; i <= 100; i++) {
+            String temp = "";
+            if (i % 3 != 0 & i % 5 != 0 & i % 7 != 0 & i / 10 != 3 & i % 10 != 3)
+                temp += i;
+            else if (i / 10 == 3 || i % 10 == 3) {
+                temp += "Fizz";
+            } else {
+                if (i % 3 == 0)
+                    temp += "Fizz";
+                if (i % 5 == 0)
+                    temp += "Buzz";
+                if (i % 7 == 0)
+                    temp += "Whizz";
+            }
+            results.add(temp);
+        }
+    }
+public void return_Fizz_when_have_3(int count) {
+    for (int i = 1; i <= 100; i++) {
+        String temp = "";
+        if (i / 10 != 3 & i % 10 != 3)
+            temp += i;
+        else if (i / 10 == 3 || i % 10 == 3) {
+            temp += "Fizz";
+        }
+        results.add(temp);
 
     }
-
+}
     public List<String> getResults() {
         return results;
     }
--- src/test/java/com/thoughtworks/FizzBuzzGameTest.java
+++ src/test/java/com/thoughtworks/FizzBuzzGameTest.java
@@ -2,9 +2,32 @@ package com.thoughtworks;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+
 public class FizzBuzzGameTest {
     @Test
     public void should_return_Fizz_when_have_3() {
+        List<String> expectedResults = Arrays.asList(
+                "1", "2", "Fizz", "4", "5", "6", "7", "8", "9", "10",
+                "11", "12", "Fizz", "14", "15", "16", "17", "18", "19", "20",
+                "21", "22", "Fizz", "24", "25", "26", "27", "28", "29", "Fizz",
+                "Fizz", "Fizz", "Fizz", "Fizz", "Fizz", "Fizz", "Fizz", "Fizz", "Fizz", "40",
+                "41", "42", "Fizz", "44", "45", "46", "47", "48", "49", "50",
+                "51", "52", "Fizz", "54", "55", "56", "57", "58", "59", "60",
+                "61", "62", "Fizz", "64", "65", "66", "67", "68", "69", "70",
+                "71", "72", "Fizz", "74", "75", "76", "77", "78", "79", "80",
+                "81", "82", "Fizz", "84", "85", "86", "87", "88", "89", "90",
+                "91", "92", "Fizz", "94", "95", "96", "97", "98", "99", "100"
+        );
 
+
+        FizzBuzzGame game = new FizzBuzzGame();
+        game.return_Fizz_when_have_3(100);
+        List<String> results = game.getResults();
+        assertThat(results, equalTo(expectedResults));
     }
 }
